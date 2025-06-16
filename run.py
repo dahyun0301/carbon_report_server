885ca6c439b1ab436df57f199ac1582ae4f4c393
k1young <kiyoung0611@naver.com>
2025-06-16 18:44:18 +0900

    불필요부분 삭제

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.main:app",   # "모듈경로:FastAPI앱인스턴스"
+        "app.main:app",   
         host="127.0.0.1",
         port=8000,
-        reload=True       # 개발 중 자동 재시작
+        reload=True     
     )
 
